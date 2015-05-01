907e5f7fab0c603ee8a47bef233defdbe1c0220c
Abram C. Isola <abram@isola.mn>
2015-05-01 00:13:21 -0500

    update greet 0.2

--- examples/greet0.2.py
+++ examples/greet0.2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+sys.path.append('../')
 import cli
 
 # the action of the program
@@ -18,7 +19,7 @@ if __name__ == "__main__":
     APP.version = "0.2"
     APP.usage   = "fight the loneliness!"
     APP.flags   = [
-        cli.Flag(cli.F_STRING, "lang,l", "english", "set the language for the greeting"),
+        cli.Flag(cli.STRING, "lang,l", "english", "set the language for the greeting"),
     ]
     APP.action  = main
     APP.run(sys.argv)
